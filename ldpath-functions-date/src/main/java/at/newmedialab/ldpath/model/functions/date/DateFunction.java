8aff68f0f68968138e59665ebab594a205b1b617
jakob frank <jakob.frank@salzburgresearch.at>
2012-09-28 14:22:23 +0200

    created ldpath-function-date package providing some date-based functions, currently: earliest, latest

---
+++ ldpath-functions-date/src/main/java/at/newmedialab/ldpath/model/functions/date/DateFunction.java
@@ -0,0 +1,7 @@
+package at.newmedialab.ldpath.model.functions.date;
+
+import at.newmedialab.ldpath.api.functions.SelectorFunction;
+
+public interface DateFunction<Node> extends SelectorFunction<Node> {
+
+}
